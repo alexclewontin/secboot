7c20b150dde1dd1472668832db062202b60da920
Frederic Hoerni <frederic.hoerni@canonical.com>
2026-07-13 10:00:57 +0200

    efi/pcr_profile: minor quality improvements
    - fix call to deprecated func
    - fix typo
    - remove an item of a list that was not used

--- efi/pcr_profile_test.go
+++ efi/pcr_profile_test.go
@@ -28,8 +28,8 @@ import (
 
 	efi "github.com/canonical/go-efilib"
 	"github.com/canonical/go-tpm2"
+	"github.com/canonical/go-tpm2/policyutil"
 	tpm2_testutil "github.com/canonical/go-tpm2/testutil"
-	"github.com/canonical/go-tpm2/util"
 	"github.com/canonical/tcglog-parser"
 	. "gopkg.in/check.v1"
 
@@ -121,7 +121,7 @@ func (s *pcrProfileMockedSuite) TestAddPCRProfileSimple(c *C) {
 	profile := secboot_tpm2.NewPCRProtectionProfile()
 
 	var digests tpm2.DigestList
-	for i := 0; i <= 2; i++ {
+	for i := 0; i <= 1; i++ {
 		h := crypto.SHA256.New()
 		io.WriteString(h, strconv.Itoa(i))
 		digests = append(digests, h.Sum(nil))
@@ -633,7 +633,7 @@ func (s *pcrProfileSuite) testAddPCRProfile(c *C, data *testAddPCRProfileData, o
 	var expectedPcrs tpm2.PCRSelectionList
 	var expectedDigests tpm2.DigestList
 	for i, v := range data.expected {
-		pcrs, digest, err := util.ComputePCRDigestFromAllValues(tpm2.HashAlgorithmSHA256, v)
+		pcrs, digest, err := policyutil.ComputePCRDigestFromAllValues(tpm2.HashAlgorithmSHA256, v)
 		c.Assert(err, IsNil)
 		if i == 0 {
 			expectedPcrs = pcrs
